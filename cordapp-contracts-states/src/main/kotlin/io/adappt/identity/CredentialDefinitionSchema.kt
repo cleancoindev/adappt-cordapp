d7bf0c29100195abd283751524d31d6680b3c913
Dom Steil <dominic@dapps-inc.com>
2019-01-28 10:24:53 -0800

    adding identity

---
+++ cordapp-contracts-states/src/main/kotlin/io/adappt/identity/CredentialDefinitionSchema.kt
@@ -0,0 +1,30 @@
+package io.adappt.identity
+
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import javax.persistence.Column
+import javax.persistence.Entity
+
+object CredentialDefinitionSchema
+
+object CredentialDefinitionSchemaV1 : MappedSchema(
+        version = 1,
+        schemaFamily = CredentialDefinitionSchema.javaClass,
+        mappedTypes = listOf(PersistentCredentialDefinition::class.java)
+) {
+    @Entity
+    data class PersistentCredentialDefinition(
+            @Column(name = "id") val credentialDefId: String = "",
+            val schemaId: String = "",
+            val revRegId: String = "",
+            val currentCredNumber: Int = 0
+
+    ) : PersistentState() {
+        constructor(credentialDef: IndyCredentialDefinition) : this(
+                credentialDef.credentialDefinitionId.toString(),
+                credentialDef.schemaId.toString(),
+                credentialDef.credentialDefinitionId.getRevocationRegistryDefinitionId(IndyUser.REVOCATION_TAG).toString(),
+                credentialDef.currentCredNumber
+        )
+    }
+}
---
+++ cordapp-contracts-states/src/main/kotlin/io/adappt/identity/IndySchema.kt
@@ -0,0 +1,31 @@
+package io.adappt.identity
+
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
+
+/**
+ * A Corda record representing indy schema
+ *
+ * @param id                id of this schema
+ * @param participants      corda participants
+ */
+class IndySchema(
+        val id: String,
+        override val participants: List<AbstractParty>
+) : LinearState, QueryableState {
+
+    override val linearId: UniqueIdentifier = UniqueIdentifier()
+
+    override fun generateMappedObject(schema: MappedSchema): PersistentState {
+        return when (schema) {
+            is IndySchemaSchemaV1 -> IndySchemaSchemaV1.PersistentSchema(this)
+            else -> throw IllegalArgumentException("Unrecognised schema: $schema")
+        }
+    }
+
+    override fun supportedSchemas() = listOf(IndySchemaSchemaV1)
+}
